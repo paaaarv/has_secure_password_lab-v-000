af6f8afa728a57c7bcbb17d9543625b2373337b7
Parv Kumar <parvathikumar92@gmail.com>
2017-12-14 22:26:06 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.new(user_params).save
 
   end
@@ -18,6 +17,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(@user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
