ce0b4313f8b5d7d9b3bb0e2d8b69d314847434c7
Parv Kumar <parvathikumar92@gmail.com>
2017-12-14 22:58:14 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
 
   def create
+    binding.pry
     @user = User.find_by(:name => params[:name])
     if @user == nil
       redirect_to '/users/new'
