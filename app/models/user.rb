7e6b4d66684b17a95b5be56a4c9e830517c4af7d
Parv Kumar <parvathikumar92@gmail.com>
2017-12-14 21:59:34 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 
+gem 'bcrypt'
 class User < ActiveRecord::Base
   has_secure_password
 
   def authenticate(password)
-    if self.password_digest == password
+
+    salt = password[0..28]
+    hashed = BCrypt::Engine::hash_secret(password, salt)
+    if self.password_digest == (salt + hashed)
       return self
     else
       return false
