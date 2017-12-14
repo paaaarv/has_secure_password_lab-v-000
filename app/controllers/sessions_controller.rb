53b1dae516d909cbae073e592c1a7249fa9c8178
Parv Kumar <parvathikumar92@gmail.com>
2017-12-14 22:58:09 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
       redirect_to '/users/new'
     else
       return head(:forbidden) unless @user.auntheticate(params[:password])
-      binding.pry
       session[:user_id] = @user.id
       redirect_to '/users'
     end
