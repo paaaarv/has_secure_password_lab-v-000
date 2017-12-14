b3aafc81c3bd8e463d9d1dcd8e53c427e28331be
Parv Kumar <parvathikumar92@gmail.com>
2017-12-14 23:04:27 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     if user_params[:password] == user_params[:password_confirmation]
       @user = User.new(user_params)
+      @user.save
       binding.pry
       redirect_to '/login'
     else
